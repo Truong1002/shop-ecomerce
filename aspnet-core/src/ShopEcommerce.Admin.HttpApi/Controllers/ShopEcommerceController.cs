b9482528b3f75677a9530482138d6fdd3c456e5d
Truong1002 <truongphamxuan724@gmail.com>
2024-04-10 23:00:12 +0700

    Finish folder structure

---
+++ aspnet-core/src/ShopEcommerce.Admin.HttpApi/Controllers/ShopEcommerceController.cs
@@ -0,0 +1,14 @@
+﻿using ShopEcommerce.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace ShopEcommerce.Admin.Controllers;
+
+/* Inherit your controllers from this class.
+ */
+public abstract class ShopEcommerceController : AbpControllerBase
+{
+    protected ShopEcommerceController()
+    {
+        LocalizationResource = typeof(ShopEcommerceResource);
+    }
+}
