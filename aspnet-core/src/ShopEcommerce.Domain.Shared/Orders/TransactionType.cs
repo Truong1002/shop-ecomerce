7db3cff062de0da4e141637fff55746b4da4ab2a
Truong1002 <truongphamxuan724@gmail.com>
2024-04-12 17:00:58 +0700

    Complete Aggregate

---
+++ aspnet-core/src/ShopEcommerce.Domain.Shared/Orders/TransactionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopEcommerce.Orders
+{
+    public enum TransactionType
+    {
+        ConfirmOrder,
+        StartProcessing,
+        FinishOrder,
+        CancelOrder
+    }
+}
